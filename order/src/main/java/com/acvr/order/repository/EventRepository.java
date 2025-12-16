51cf076c452a27122825d4f339466e70e0a61e19
manjunathprasadgr <manjunatha.prasad@acuverconsulting.com>
2025-12-16 10:31:33 +0530

    Delete order directory

--- order/src/main/java/com/acvr/order/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.acvr.order.repository;
-
-import com.acvr.order.event.EventDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EventRepository extends MongoRepository<EventDocument, String> {
-}
--- order/src/main/java/com/acvr/order/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.acvr.order.repository;
-
-import com.acvr.order.dto.OrderDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrderDto, Long> {
-
-}
